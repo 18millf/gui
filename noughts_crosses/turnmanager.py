99ff488825a85ddacbd54457cdcd8223f1ae2381
Millzy <millzyyt08@gmail.com>
2023-12-11 14:36:04 +0000

    fix typing

--- noughts_crosses/turnmanager.py
+++ noughts_crosses/turnmanager.py
@@ -3,8 +3,8 @@ from typing import TypeVar, Generic
 T = TypeVar("T")
 
 class TurnManager(Generic[T]):
-    def __init__(self, turns: list(T)) -> None:
-        self.__turns: list(T) = turns
+    def __init__(self, turns: list[T]) -> None:
+        self.__turns: list[T] = turns
         self.__current_turn: int = 0
         self.__turn_count = len(self.__turns)
 
