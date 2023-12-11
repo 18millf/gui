73b210ae2d5407232bd6a463887253fc89428d00
Millzy <millzyyt08@gmail.com>
2023-12-11 14:51:56 +0000

    comments

--- noughts_crosses/main.py
+++ noughts_crosses/main.py
@@ -12,7 +12,7 @@ class GameView(BoxLayout):
         super().__init__(**kwargs)
 
         self.__game: Game = Game()
-        self.__turn_manager = TurnManager([Placement.NOUGHT, Placement.CROSS])
+        self.__turn_manager = TurnManager([Placement.NOUGHT, Placement.CROSS]) # Only two
         self.__buttons: list[Button] = list()
 
         self.orientation = "vertical"
@@ -36,30 +36,31 @@ class GameView(BoxLayout):
 
         
     def create_button_callback(self, row: int, col: int):
-        return lambda button: self.play_turn(button, row, col)
+        return lambda button: self.play_turn(button, row, col) # creates a lambda for the coordinates of the created button
     
     def play_turn(self, button: Button, row: int, col: int):
-        button.disabled = True
+        button.disabled = True # prevent the grid square from being pressed twice
 
-        turn: Placement = self.__turn_manager.get_current_turn()
+        turn: Placement = self.__turn_manager.get_current_turn() 
 
-        button.text = "O" if turn == Placement.NOUGHT else "X"
+        button.text = "O" if turn == Placement.NOUGHT else "X" 
 
-        self.__game.place(turn, row, col)
+        self.__game.place(turn, row, col) # place the turn in the backend
 
         if self.__game.game_is_over():
             for btn in self.__buttons:
-                btn.disabled = True
+                btn.disabled = True # prevent continued play once the game is over
 
             winner: Placement = self.__game.get_winner()
 
-            if winner == Placement.NONE:
+            if winner == Placement.NONE: # the winner is NONE if its a draw
                 self.__dialogue.text = "Its a draw!"
+                return
 
             self.__dialogue.text = ("Noughts" if winner == Placement.NOUGHT else "Crosses") + " Wins!"
             return
 
-        self.__turn_manager.advance_turn()
+        self.__turn_manager.advance_turn() #  next turn
         self.__dialogue.text = ("Noughts" if self.__turn_manager.get_current_turn() == Placement.NOUGHT else "Crosses") + " turn."
 
 class NoughtsAndCrossesApp(App):
--- noughts_crosses/turnmanager.py
+++ noughts_crosses/turnmanager.py
@@ -2,14 +2,14 @@ from typing import TypeVar, Generic
 
 T = TypeVar("T")
 
-class TurnManager(Generic[T]):
+class TurnManager(Generic[T]): # i wanted an excuse to use generics...
     def __init__(self, turns: list[T]) -> None:
         self.__turns: list[T] = turns
         self.__current_turn: int = 0
         self.__turn_count = len(self.__turns)
 
     def get_current_turn(self) -> T:
-        return self.__turns[self.__current_turn % self.__turn_count]
+        return self.__turns[self.__current_turn % self.__turn_count] # keeps the turn in the limits of the array of turns
     
     def advance_turn(self) -> None:
-        self.__current_turn += 1
+        self.__current_turn += 1 # increment the turn index
